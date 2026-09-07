409a8aad1dd5b7484e4b431d2cbd6830faa0d3c8
basit-of-africa <ajibadebasit40@gmail.com>
2026-09-07 14:43:30 +0100

    new commit

--- web/.next/static/chunks/webpack.js
+++ web/.next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "9c5ee789fb4f478c"; }
+/******/ 		__webpack_require__.h = function() { return "a48a21c16ab9f10b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
