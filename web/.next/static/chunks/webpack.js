901be86692bb107803c32e03004548d10ffc1ca5
basit-of-africa <ajibadebasit40@gmail.com>
2026-09-10 22:07:02 +0100

    feat: update hot-update files and hashes for improved functionality

--- web/.next/static/chunks/webpack.js
+++ web/.next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "7cf200a97d225fe6"; }
+/******/ 		__webpack_require__.h = function() { return "fd09a21e6a92f3c1"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
