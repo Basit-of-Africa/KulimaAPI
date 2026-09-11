60a5e6e8b6921d3ee0246b1bc04e8b8605bf65f2
basit-of-africa <ajibadebasit40@gmail.com>
2026-09-11 01:11:09 +0100

    Add initial hot-update JSON file for Webpack static assets

--- web/.next/server/webpack-runtime.js
+++ web/.next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("7b70021d683607bb")
+/******/ 		__webpack_require__.h = () => ("a8f19cf907c221d7")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
